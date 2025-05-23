c47baddd37d86049bd0fc16077e83e891f1ebbd9
lmy <2075945685@qq.com>
2025-05-23 10:47:14 +0800

    283-移动零

--- src/com/li/leetcode/hot100_283移动零.java
+++ src/com/li/leetcode/hot100_283移动零.java
@@ -2,6 +2,10 @@ package com.li.leetcode;
 
 public class hot100_283移动零 {
     public void moveZeroes(int[] nums) {
+
+        /**
+         * 冒泡排序
+         */
         for(int i =0;i<nums.length;i++){
             for (int j = i+1; j < nums.length; j++) {
                 int temp;
@@ -12,5 +16,25 @@ public class hot100_283移动零 {
                 }
             }
         }
+
+        /**
+         * 快慢指针
+         * 1.lift和right指向同一个位置
+         * 2.当right不等于0时，交换左右位置，左右指针右移
+         * 3.当right等于0时，左指针不动，右指针右移
+         */
+        int lift=0,right=0;
+     while (right<nums.length){
+         if(nums[right]!=0){
+             swap(nums,lift,right);
+             lift++;
+         }
+         right++;
+     }
+    }
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
